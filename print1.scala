7e3793bd433de884a90ffd42ce0503debdd8ab5f
mjain06 <92713031+mjain06@users.noreply.github.com>
2022-01-04 17:41:22 +0100

    Create print1.scala

---
+++ print1.scala
@@ -0,0 +1,25 @@
+package com.wehkamp
+
+import akka.actor.{ActorRef, ActorSystem}
+import com.wehkamp.ActorConstants.actorSystemName
+import com.wehkamp.service.{BasketService, CatalogService}
+import play.api.inject._
+import play.api.{Configuration, Environment}
+
+class AppModule extends Module {
+
+  implicit private val ec = new ExecutionContexts().ec
+
+  private val system = ActorSystem(actorSystemName)
+  private val actorContext = new ActorContext(system)
+
+  def bindings(environment: Environment, configuration: Configuration) =
+    Seq(
+      bind[ExecutionContexts].toInstance(new ExecutionContexts),
+
+      bind[CatalogService].toSelf,
+      bind[BasketService].toSelf,
+
+      bind[ActorContext].toInstance(actorContext),
+      bind[ActorRef].qualifiedWith("catalogActor").toInstance(actorContext.catalogActor),
+      bind[ActorRef].qualifiedWith("basketActor").toInstance(actorContext.basketActor()))
